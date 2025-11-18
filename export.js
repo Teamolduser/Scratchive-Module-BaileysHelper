57cd06deb82dad694918ec0c6378a392640b3edd
Old_usr <120130487+Teamolduser@users.noreply.github.com>
2025-11-18 17:21:11 +0530

    Update export.js

--- export.js
+++ export.js
@@ -1,24 +1,13 @@
-/**
- * Central export surface for this package.
- * Re-exports helper functions from local modules and exposes package metadata.
- */
+import pkg from './package.json' with { type: 'json' };
 
-const buttons = require('./helpers/buttons');
-const pkg = require('./package.json');
+export * from './helpers/buttons.js';
 
-const getPackageInfo = () => ({
-	name: pkg.name,
-	version: pkg.version,
-	description: pkg.description,
-	author: pkg.author,
-	main: pkg.main,
+export const getPackageInfo = () => ({
+  name: pkg.name,
+  version: pkg.version,
+  description: pkg.description,
+  author: pkg.author,
+  main: pkg.main,
 });
 
-module.exports = {
-	// re-export all named helpers from buttons.js (sendInteractiveButtonsBasic, sendButtons, ...)
-	...buttons,
-
-	// package metadata convenience
-	pkg,
-	getPackageInfo,
-};
+export { pkg };
